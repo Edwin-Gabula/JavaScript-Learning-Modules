e26178a4418d1cefab06746ab6685375ae78b849
Hyper-Realism <hyper-realism@Hyper-Realisms-MacBook-Air.local>
2026-08-08 00:02:25 +0300

    Adds to the basics folder

---
+++ 01-Basics/Exercise/01-console.js
@@ -0,0 +1,17 @@
+/*
+program to print my names,
+what I am doing,
+and why
+*/
+
+console.log(
+    "My name is John Dowe"
+);
+
+console.log(
+    "I am leaning JS"
+);
+
+console.log(
+    "Because I want to become a web developer with JS"
+)
