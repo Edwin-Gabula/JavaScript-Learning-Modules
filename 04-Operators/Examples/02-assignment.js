5c1aa44ae43293b6cf94c8ef42d11a69ac4541d2
Hyper-Realism <hyper-realism@Hyper-Realisms-MacBook-Air.local>
2026-08-10 13:09:46 +0300

    Added to the operators folder

---
+++ 04-Operators/Examples/02-assignment.js
@@ -0,0 +1,44 @@
+/*
+assignment operators
+*/
+
+let stock = 500;
+
+// printing the current stock available
+console.log(
+    "Current stock: ", stock
+);
+
+// adding stock to the available one
+
+stock += 150
+
+console.log(
+    "New stock available: ", stock
+);
+
+
+// after dispensing some drugs
+
+stock -= 75;
+
+console.log(
+    "After dispensing: ", stock
+);
+
+
+// after multiplication
+stock *= 2
+
+console.log(
+    "After multiplication: ", stock
+);
+
+
+// remainder after some divisions
+
+stock %= 7;
+
+console.log(
+    "Remainder after division: ", stock
+);
---
+++ 04-Operators/Examples/03-comparison.js
@@ -0,0 +1,44 @@
+/*
+comparison operators
+*/
+
+const stock = 500;
+
+const reorderLevel = 600;
+
+
+// the normal comparison operators...
+
+console.log(
+    "Stock > Reorder level: ", stock > reorderLevel
+);
+
+console.log(
+    "Stock < Reorder level: ", stock < reorderLevel
+);
+
+console.log(
+    "Stock >= Reorder level: ", stock >= reorderLevel
+);
+
+console.log(
+    "Stock <= Reorder: ", stock <= reorderLevel
+);
+
+
+console.log(
+    "Stock == 500", stock == 500
+);
+
+console.log(
+    "Stock === 500", stock === 500
+);
+
+
+console.log(
+    "Stock != 500", stock != 500
+);
+
+console.log(
+    "Stock !== 500", stock !== 500
+);
